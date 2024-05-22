737bb7b5505fbf5d2cfb82999beb9fcfaa5a29c9
Chris <52449218+shadow578@users.noreply.github.com>
2024-05-22 08:12:55 +0000

    update references to integration_blueprint

--- custom_components/tvxml_epg/__init__.py
+++ custom_components/tvxml_epg/__init__.py
@@ -1,7 +1,7 @@
-"""Custom integration to integrate integration_blueprint with Home Assistant.
+"""Custom integration to integrate TV XML EPG data with Home Assistant.
 
 For more details about this integration, please refer to
-https://github.com/ludeeus/integration_blueprint
+https://github.com/shadow578/homeassistant_tvxml-epg
 """
 from __future__ import annotations
 
--- custom_components/tvxml_epg/coordinator.py
+++ custom_components/tvxml_epg/coordinator.py
@@ -1,4 +1,4 @@
-"""DataUpdateCoordinator for integration_blueprint."""
+"""DataUpdateCoordinator for tvxml_epg."""
 from __future__ import annotations
 
 from datetime import datetime, timedelta
