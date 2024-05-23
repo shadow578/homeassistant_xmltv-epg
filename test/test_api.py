1fe2cbade3f89a20ef43cb19c9f477d641501e79
Chris <52449218+shadow578@users.noreply.github.com>
2024-05-23 13:09:48 +0000

    add tests for XMLTVClient

---
+++ test/test_api.py
@@ -0,0 +1,149 @@
+"""Test xmltv_epg api component."""
+
+from datetime import datetime, timedelta
+
+import aiohttp
+import gzip
+
+import pytest
+
+from homeassistant.const import CONF_HOST
+
+from unittest.mock import MagicMock, AsyncMock
+from pytest_homeassistant_custom_component.common import MockConfigEntry
+
+from custom_components.xmltv_epg.api import XMLTVClient
+from custom_components.xmltv_epg.const import DOMAIN, OPT_PROGRAM_LOOKAHEAD
+
+from custom_components.xmltv_epg.xmltv.model import TVGuide, TVChannel, TVProgram
+
+def create_mock_session_for_get():
+    """Create a mock session that supports get().
+
+    :return: (mock session, mock response)"""
+
+    response = AsyncMock()
+    response.raise_for_status = MagicMock()
+
+    session = AsyncMock(spec=aiohttp.ClientSession)
+    session.get = AsyncMock(return_value=response)
+
+    return session, response
+
+
+async def test_xmltv_client_get_data_plain(
+        anyio_backend
+    ):
+    """Test XMLTVClient.async_get_data with plain XML response."""
+
+    # prepare the session and response
+    session, response = create_mock_session_for_get()
+
+    response.content_type = "text/xml"
+    response.url = "http://example.com/epg.xml"
+    response.text.return_value = """
+<tv generator-info-name="xmltv_epg" generator-info-url="http://example.com">
+  <channel id="CH1">
+    <display-name>Channel 1</display-name>
+    </channel>
+    <programme start="20200101010000 +0000" stop="20200101020000 +0000" channel="CH1">
+        <title>Program 1</title>
+        <desc>Description 1</desc>
+    </programme>
+</tv>
+"""
+
+    # create client
+    client = XMLTVClient(
+        session=session,
+        url="http://example.com/epg.xml",
+    )
+
+    # fetch data
+    guide = await client.async_get_data()
+
+    # check the guide
+    assert guide
+    assert guide.generator_name == "xmltv_epg"
+    assert guide.generator_url == "http://example.com"
+
+    assert len(guide.channels) == 1
+    assert guide.channels[0].id == "CH1"
+
+async def test_xmltv_client_get_data_gzip_correct_content_type(
+        anyio_backend
+    ):
+    """Test XMLTVClient.async_get_data with GZIP response and correct content_type."""
+
+    # prepare the session and response
+    session, response = create_mock_session_for_get()
+
+    response.content_type = "application/gzip"
+    response.url = "http://example.com/epg.xml.gz"
+    response.read.return_value = gzip.compress("""
+<tv generator-info-name="xmltv_epg" generator-info-url="http://example.com">
+  <channel id="CH1">
+    <display-name>Channel 1</display-name>
+    </channel>
+    <programme start="20200101010000 +0000" stop="20200101020000 +0000" channel="CH1">
+        <title>Program 1</title>
+        <desc>Description 1</desc>
+    </programme>
+</tv>
+""".encode())
+
+    # create client
+    client = XMLTVClient(
+        session=session,
+        url="http://example.com/epg.xml.gz",
+    )
+
+    # fetch data
+    guide = await client.async_get_data()
+
+    # check the guide
+    assert guide
+    assert guide.generator_name == "xmltv_epg"
+    assert guide.generator_url == "http://example.com"
+
+    assert len(guide.channels) == 1
+    assert guide.channels[0].id == "CH1"
+
+async def test_xmltv_client_get_data_gzip_wrong_content_type(
+        anyio_backend
+    ):
+    """Test XMLTVClient.async_get_data with GZIP response and wrong content_type."""
+
+    # prepare the session and response
+    session, response = create_mock_session_for_get()
+
+    response.content_type = "application/octet-stream"
+    response.url = "http://example.com/epg.xml.gz"
+    response.read.return_value = gzip.compress("""
+<tv generator-info-name="xmltv_epg" generator-info-url="http://example.com">
+  <channel id="CH1">
+    <display-name>Channel 1</display-name>
+    </channel>
+    <programme start="20200101010000 +0000" stop="20200101020000 +0000" channel="CH1">
+        <title>Program 1</title>
+        <desc>Description 1</desc>
+    </programme>
+</tv>
+""".encode())
+
+    # create client
+    client = XMLTVClient(
+        session=session,
+        url="http://example.com/epg.xml.gz",
+    )
+
+    # fetch data
+    guide = await client.async_get_data()
+
+    # check the guide
+    assert guide
+    assert guide.generator_name == "xmltv_epg"
+    assert guide.generator_url == "http://example.com"
+
+    assert len(guide.channels) == 1
+    assert guide.channels[0].id == "CH1"
