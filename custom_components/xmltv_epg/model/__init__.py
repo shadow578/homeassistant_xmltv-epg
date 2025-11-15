f7efbc7a04beb9d4b7c73c14e5e600502db2f8c7
shadow578 <52449218+shadow578@users.noreply.github.com>
2025-11-15 14:53:13 +0000

    start refactor of model parsing using pydantic_xml

--- custom_components/xmltv_epg/model/__init__.py
+++ custom_components/xmltv_epg/model/__init__.py
@@ -1,11 +1,13 @@
-"""XMLTV Model."""
+"""XMLTV EPG model and parsing."""
 
 from .channel import TVChannel
+from .episode_number import TVProgramEpisodeNumber
 from .guide import TVGuide
 from .program import TVProgram
 
 __all__ = [
     "TVChannel",
+    "TVProgramEpisodeNumber",
     "TVProgram",
     "TVGuide",
 ]
---
+++ custom_components/xmltv_epg/model/episode_number.py
@@ -0,0 +1,57 @@
+"""Model for TV program episode numbering handling."""
+
+from typing import Any
+
+from pydantic_xml import BaseXmlModel, attr
+
+
+class TVProgramEpisodeNumber(BaseXmlModel, tag="episode-num"):
+    """Represents an episode number in a specific numbering system."""
+
+    system: str = attr(name="system")
+    """The numbering system used (e.g. "SxxExx", "xmltv_ns")."""
+
+    raw_value: str
+    """The raw episode number value as a string. Format depends on the numbering system."""
+
+    @property
+    def value(self) -> tuple[int | None, int | None]:
+        """Parse the raw episode number into a (season, episode) tuple.
+
+        :return: A tuple of (season, episode), where either (or both) can be None if not available.
+        """
+        return self.__value
+
+    def __parse_value(self) -> tuple[int | None, int | None]:
+        """Parse the raw_value based on the system. Used once after model initialization."""
+        if self.system == "SxxExx" or self.system == "onscreen":
+            val = self.raw_value.upper()
+
+            # valid formats: S#E#, S#, E#
+            if val.startswith("E"):
+                # episode only format E##
+                episode = int(val[1:])
+                return (None, episode)
+
+            # season+episode format S##E## or season only format S##
+            (season, episode) = val.split("E")
+            season = int(season[1:])
+            episode = int(episode) if episode else None
+            return (season, episode)
+
+        if self.system == "xmltv_ns":
+            # xmltv_ns format is '0.0.' for 'S1E1'
+            # alternative format is '.0.' for episode only
+            s, e, _ = self.raw_value.split(".")
+
+            season = int(s) + 1 if s else None
+            episode = int(e) + 1 if e else None
+            return (season, episode)
+
+        # cannot parse
+        return (None, None)
+
+    def model_post_init(self, __context: Any) -> None:
+        """Hooks post-initialization to parse the raw value."""
+        self.__value = self.__parse_value()
+        return super().model_post_init(__context)
